c4602efe656507cc1a848134ef26a0af226c3f17
Gamaliel <Gammy1524@gmail.com>
2017-05-15 12:08:32 -0700

    done

---
+++ quiz.js
@@ -0,0 +1,81 @@
+function problemOne() {
+  var selection = document.querySelector("input[name='programmer']:checked").value
+  if(selection == "5"){
+    alert("correct")
+  }else{
+    alert("you're wrong!")
+  }
+}
+
+function problemTwo() {
+  var selection = document.querySelector("input[name='programmer']:checked").value
+  if(selection == "two"){
+    alert("correct")
+  }else{
+    alert("you're wrong!")
+  }
+}
+function problemThree() {
+  var selection = document.querySelector("input[name='programmer']:checked").value
+  if(selection == "81"){
+    alert("correct")
+  }else{
+    alert("you're wrong!")
+  }
+}
+function problemFour() {
+  var selection = document.querySelector("input[name='programmer']:checked").value
+  if(selection == "Bulls"){
+    alert("correct")
+  }else{
+    alert("you're wrong!")
+  }
+}
+function problemFive() {
+  var selection = document.querySelector("input[name='programmer']:checked").value
+  if(selection == "Cavs,Heat"){
+    alert("correct")
+  }else{
+    alert("you're wrong!")
+  }
+}
+function problemSix() {
+  var selection = document.querySelector("input[name='programmer']:checked").value
+  if(selection == "Germany"){
+    alert("correct")
+  }else{
+    alert("you're wrong!")
+  }
+}
+function problemSeven() {
+  var selection = document.querySelector("input[name='programmer']:checked").value
+  if(selection == "Bulls,Wizards"){
+    alert("correct")
+  }else{
+    alert("you're wrong!")
+  }
+}
+function problemEight() {
+  var selection = document.querySelector("input[name='programmer']:checked").value
+  if(selection == "Jordan"){
+    alert("correct")
+  }else{
+    alert("you're wrong!")
+  }
+}
+function problemNine() {
+  var selection = document.querySelector("input[name='programmer']:checked").value
+  if(selection == "3"){
+    alert("correct")
+  }else{
+    alert("you're wrong!")
+  }
+}
+function problemTen() {
+  var selection = document.querySelector("input[name='programmer']:checked").value
+  if(selection == "Russel"){
+    alert("correct")
+  }else{
+    alert("you're wrong!")
+  }
+}
